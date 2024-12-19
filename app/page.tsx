3a6dbf0392f7689d96fbc4258a1326ebe972ce60
akinolapo <akinolapo@gmail.com>
2024-12-19 06:52:37 -0600

    coin list

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import CoinList from "./components/CoinList";
 
 export default function Home() {
   return (
     <>
     <HeroSection />
+    <CoinList />
     </>
   );
 }
