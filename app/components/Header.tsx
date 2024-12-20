fae7bba516d3c68e712f7e9632834a07a765752b
akinolapo <akinolapo@gmail.com>
2024-12-20 00:44:46 -0600

    Cryptocurrencies page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,7 @@
 import { FaUserCircle, FaBars } from 'react-icons/fa';
 import { useState } from 'react';
 import { FaBitcoin } from 'react-icons/fa6';
+import Link from 'next/link';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -17,7 +18,7 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center py-4 px-4 lg:px-0">
         {/* Left Section */}
         <div className="flex items-center">
-          <div className="text-2xl font-bold flex items-center gap-2"><FaBitcoin/> SumoTech</div>
+          <Link href="/" className="text-2xl font-bold flex items-center gap-2"><FaBitcoin/> SumoTech</Link>
           <nav className="hidden lg:flex ml-8 space-x-6">
             <a href="#" className="hover:text-primary">Cryptocurrencies</a>
             <a href="#" className="hover:text-primary">Exchanges</a>
@@ -32,10 +33,14 @@ const Header = () => {
             placeholder="Search..."
             className="px-3 py-2 rounded-md bg-[#141b29] text-white border border-[#2a3b4f] focus:outline-none focus:border-primary"
           />
+          <Link href="/signup">
           <button className="bg-primary px-4 py-2 rounded-md hover:bg-[#00b385] transition">
             Sign Up
           </button>
+          </Link>
+          <Link href="/account">
           <FaUserCircle size={28} className="cursor-pointer" />
+          </Link>
         </div>
 
         {/* Mobile Menu Icon */}
